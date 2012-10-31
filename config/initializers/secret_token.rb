6799bd07f3942e18450eb0690396358a6c838e03
Rainux Luo <rainux@gmail.com>
2012-10-31 18:25:47 +0800

    Rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetherLy::Application.config.secret_token = '788c2035de69e90cd1a87ddd563895829993ef6586029411c44eda79e789e47526ac246b8e83f6efb6e755a58ebceefac19f22e36df2b8680724698b3e3befc3'
