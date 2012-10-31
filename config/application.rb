3e5bce47c4abe07c4f686b2177c0a545f1c37b11
Rainux Luo <rainux@gmail.com>
2012-10-31 18:35:05 +0800

    Configure RSpec stack

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,22 @@ require 'sprockets/railtie'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(assets: %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
 
 module GetherLy
   class Application < Rails::Application
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.controller_specs false
+      g.view_specs false
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
